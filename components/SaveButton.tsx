eb9c51f85d89907177e563e1b27739502f6d3efb
TGW/DM <tyrellgws99@gmail.com>
2026-02-15 16:17:09 +0000

    Added save functions for adding data to db in saveButton component. Added expo-sqlite to project.

--- components/SaveButton.tsx
+++ components/SaveButton.tsx
@@ -1,12 +1,43 @@
 import { StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import ThemedText from './ThemedText'
+import * as SQLite from 'expo-sqlite';
 
 interface SaveButtonProps { // Define props interface for save button
     onPress: () => void;
     text: string;
     enabled?: boolean;
 }
+
+async function setupDatabase() {
+    // Open or create the database
+    const db = await SQLite.openDatabaseAsync('events.db');
+    // Create the events table if it doesn't exist
+    await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            event_date DATE NOT NULL,
+            event_time TIME NOT NULL,
+            description TEXT
+        );
+    `);
+    return db;
+}
+
+// Function to add an event to the database
+export async function addEvent(title: string, eventDate: string, eventTime: string,  description: string) {
+    // Get the database connection
+    const db = await setupDatabase();
+    // Insert the event into the database
+    await db.runAsync(`
+        INSERT INTO events (title, event_date, event_time, description)
+        VALUES (?, ?, ?, ?);
+    `, [title, eventDate, eventTime, description]);
+    // Close the database connection
+    await db.closeAsync();
+}
+
 
 const SaveButton = ({ onPress, text = "Save", enabled = true }: SaveButtonProps) => {
     return (
